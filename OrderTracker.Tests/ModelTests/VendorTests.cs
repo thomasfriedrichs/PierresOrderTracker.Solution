61524f688874fc8f9d951f28bf6999b77eb6353c
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-14 11:03:27 -0700

    add failing test

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -19,5 +19,14 @@ namespace OrderTracker.Tests
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Name";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+    }
   }
 }
