0453da84dbc15cf1f68514d9d494c09d21a21a93
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-15 07:56:03 -0700

    add post method

--- OrderTracker/Controllers/VendorController.cs
+++ OrderTracker/Controllers/VendorController.cs
@@ -14,10 +14,17 @@ namespace OrderTracker
       return View(allVendors);
     }
 
-    [HttpGet("/categories/new")]
+    [HttpGet("/vendors/new")]
     public ActionResult New()
     {
       return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName,string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
     }
   }
 }
